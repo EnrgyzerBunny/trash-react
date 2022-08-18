c8edd42afe4a8d3fa27a9d2565ec8956f8db04ea
EnrgyzerBunny <JRWatson90@gmail.com>
2022-08-18 16:15:05 -0700

    Bugfix

--- src/components/FeaturedPlayer.tsx
+++ src/components/FeaturedPlayer.tsx
@@ -36,7 +36,7 @@ function FeaturedPlayer(props: any) {
                     setIsLoaded(true);
 
                     if (result.length > 0) {
-                        var index = Math.floor(ran.quick() * result.length);
+                        var index = Math.round(ran.quick() * result.length);
                         setPlayer(result[index]);
                         setPlayerImg("https://cdn.cloudflare.steamstatic.com/apps/dota2/players/" + result[index].AccountID + ".png");
                     }
@@ -47,71 +47,77 @@ function FeaturedPlayer(props: any) {
                     setError(error.message);
                 }
             )
-    }, []);
+    }, [ran]);
 
-    return (
-        <ContentPanel>Today's Featured Player:
-            <div className="flex flex-col min-w-full py-2">
-                <table className="flex-auto table-fixed">
-                    <tr>
-                        <td className="w-1/5">
-                            <img src={playerImg} alt="" className="bg-stone-600 shadow-lg" />
-                        </td>
-                        <td className="w-3/5 align-top px-4">
-                            <table className="w-full table-fixed border-collapse">
-                                <tr className="">
-                                    <td className="flex-auto text-left">
-                                        <div className="font-bold text-xl">
-                                            {player.PlayerName}
-                                        </div>
-                                    </td>
-                                    <td className="flex-none px-2 w-12">
-                                        <a href={"https://www.opendota.com/players/" + player.AccountID}>
-                                            <img src={ODLogo} alt="OpenDota" className="w-8" />
-                                        </a>
-                                    </td>
-                                    <td className="flex-none w-9">
-                                        <a href={"https://www.dotabuff.com/esports/players/" + player.AccountID}>
-                                            <img src={DBLogo} alt="Dotabuff" className="w-8" />
-                                        </a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <div className="text-l">
-                                            {player.ProTeamName}
-                                        </div>
-                                    </td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <div className="text-l">
-                                            {Role(player.FantasyRole)}
-                                        </div>
-                                    </td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <div className="text-l">
-                                            {(player.TeamName != null) ? player.TeamName : "Free Agent"}
-                                        </div>
-                                    </td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
+    if (error) {
+        return <ContentPanel><div>Error: {error.message}</div></ContentPanel>;
+    } else if (!isLoaded) {
+        return <ContentPanel><div>Loading...</div></ContentPanel>;
+    } else {
+        return (
+            <ContentPanel>Today's Featured Player:
+                <div className="flex flex-col min-w-full py-2">
+                    <table className="flex-auto table-fixed">
+                        <tr>
+                            <td className="w-1/5">
+                                <img src={playerImg} alt="" className="bg-stone-600 shadow-lg" />
+                            </td>
+                            <td className="w-3/5 align-top px-4">
+                                <table className="w-full table-fixed border-collapse">
+                                    <tr className="">
+                                        <td className="flex-auto text-left">
+                                            <div className="font-bold text-xl">
+                                                {player.PlayerName}
+                                            </div>
+                                        </td>
+                                        <td className="flex-none px-2 w-12">
+                                            <a href={"https://www.opendota.com/players/" + player.AccountID}>
+                                                <img src={ODLogo} alt="OpenDota" className="w-8" />
+                                            </a>
+                                        </td>
+                                        <td className="flex-none w-9">
+                                            <a href={"https://www.dotabuff.com/esports/players/" + player.AccountID}>
+                                                <img src={DBLogo} alt="Dotabuff" className="w-8" />
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <div className="text-l">
+                                                {player.ProTeamName}
+                                            </div>
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <div className="text-l">
+                                                {Role(player.FantasyRole)}
+                                            </div>
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <div className="text-l">
+                                                {(player.TeamName != null) ? player.TeamName : "Free Agent"}
+                                            </div>
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
 
-                </table>
+                    </table>
 
-            </div>
-        </ContentPanel>
-    );
+                </div>
+            </ContentPanel>
+        );
+    }
 };
 
 export default FeaturedPlayer;
