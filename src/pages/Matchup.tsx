eba10b3cc34d7a5cde50a69145c383a3dca4552a
EnrgyzerBunny <JRWatson90@gmail.com>
2022-10-16 10:33:54 -0400

    Matchup Page rework

--- src/pages/Matchup.tsx
+++ src/pages/Matchup.tsx
@@ -3,9 +3,11 @@ import PageWrapper from '../components/PageWrapper';
 import ContentPanel from '../components/ContentPanel';
 import { Link } from 'react-router-dom';
 import Chip from '../components/Chip';
+import { IsAuth } from '../auth';
 
 function MatchupPage() {
     const [matchups, setMatchups]: any = useState([]);
+    const [playerRows, setPlayerRows]: any = useState([]);
     const [error, setError]: any = useState(null);
     const [isLoaded, setIsLoaded]: any = useState(false);
 
@@ -22,41 +24,129 @@ function MatchupPage() {
         }
     };
 
+    function GetTeamRows(teamID: any) {
+        //console.log("searching " + teamID + "| team rows " + playerRows.length);
+        for (var i = 0; i < playerRows.length; i++) {
+            if (playerRows[i].TeamID === teamID) {
+                return playerRows[i];
+            }
+        }
+
+        return null;
+    }
+
+    function UserMatchup(index: any) {
+
+        if (matchups[index] == null || playerRows.length <= 0) {
+            return (<></>);
+        }
+
+        //console.log(JSON.stringify(matchups[index]));
+
+        var teamA = GetTeamRows(matchups[index].TeamA_ID);
+        var teamB = GetTeamRows(matchups[index].TeamB_ID);
+
+        if (teamA === null) {
+            //console.log("Team A Null");
+            return (<></>);
+        }
+
+        return (
+            <div>
+                <table className="flex-auto table-fixed w-full border-collapse ">
+
+                    <tbody>
+                        <tr key={"row" + teamA.TeamID + "vs" + teamB.TeamID}>
+                            <td key={"teamname-a" + teamA.TeamID} className="w-1/2 px-2 py-2 text-lg">{teamA.TeamName} - {teamA.Points.toFixed(2)}</td>
+                            <td key={"teamname-a" + teamB.TeamID} className="w-1/2 text-right px-2 py-2 text-lg">{teamB.Points.toFixed(2)} - {teamB.TeamName}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className="flex-auto table-auto w-full border-collapse border border-stone-500">
+                    <thead>
+                        <tr>
+                            <th className=" border border-stone-600 bg-stone-600 text-sm">Player</th>
+                            <th className=" border border-stone-600 bg-stone-600 text-sm">Team</th>
+                            <th className=" border border-stone-600 bg-stone-600 text-sm">Games</th>
+                            <th className=" border border-stone-600 bg-stone-600 text-sm">Points</th>
+                            <th className=" border border-stone-600 bg-stone-600 text-sm"></th>
+                            <th className=" border border-stone-600 bg-stone-600 text-sm">Points</th>
+                            <th className=" border border-stone-600 bg-stone-600 text-sm">Games</th>
+                            <th className=" border border-stone-600 bg-stone-600 text-sm">Team</th>
+                            <th className=" border border-stone-600 bg-stone-600 text-sm">Player</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {teamA.Players.map((item: any, i: any) => (
+                            <tr key={"row" + item.PlayerID + "-" + teamB.Players[i].PlayerID}>
+                                <td key={"name-a" + item.PlayerID} className="border w-2/12 px-2 py-2 border-stone-600 font-normal text-sm"><Link to={"/player/" + item.PlayerID}><Chip textSize="sm">{item.PlayerName}</Chip></Link></td>
+                                <td key={"team-a" + item.PlayerID} className="border w-1/12 text-center px-2 py-2 border-stone-600 font-normal text-sm">{item.ProTeamTag}</td>
+                                <td key={"points-a" + item.PlayerID} className="border w-1/12 text-center px-2 py-2 border-stone-600 font-normal text-sm">{item["Result Rows"]}</td>
+                                <td key={"rows-a" + item.PlayerID} className="border w-1/12 text-center px-2 py-2 border-stone-600 font-normal text-sm">{item.Points}</td>
+                                <td key={"vs" + item.PlayerID} className="border w-1/12 text-center px-2 py-2 border-stone-600 bg-stone-600 font-normal text-sm">{Role(item.FantasyRole)}</td>
+                                <td key={"rows-b" + item.PlayerID} className="border w-1/12 text-center px-2 py-2 border-stone-600 font-normal text-sm">{teamB.Players[i].Points}</td>
+                                <td key={"points-b" + item.PlayerID} className="border w-1/12 text-center px-2 py-2 border-stone-600 font-normal text-sm">{teamB.Players[i]["Result Rows"]}</td>
+                                <td key={"team-b" + item.PlayerID} className="border w-1/12 text-center px-2 py-2 border-stone-600 font-normal text-sm">{teamB.Players[i].ProTeamTag}</td>
+                                <td key={"name-b" + item.PlayerID} className="border w-2/12 px-2 py-2 border-stone-600 font-normal text-sm"><Link to={"/player/" + teamB.Players[i].PlayerID}><Chip align="end" textSize="sm">{teamB.Players[i].PlayerName}</Chip></Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+
+    };
+
     useEffect(() => {
         //console.log("called");
         fetch("https://sea.ddns.net/api/matchup?season=4")
             .then(res => res.json())
             .then(
                 (result) => {
-                    setIsLoaded(true);
-                    if (result.length > 0) {
+
+                    var playerRows = result.Rows;
+                    if (playerRows.length > 0) {
                         var compiled: any = [];
                         var hashed: any = [];
-                        for (var i = 0; i < result.length; i++)
-                        {   
-                            if (!hashed[result[i].TeamID])
-                            {
+                        for (var i = 0; i < playerRows.length; i++) {
+                            if (!hashed[playerRows[i].TeamID]) {
                                 compiled.push({
-                                    "TeamName": result[i].TeamName,
-                                    "OwnerID": result[i].OwnerID,
-                                    "OwnerName": result[i].OwnerName,
-                                    "TeamID": result[i].TeamID,
+                                    "TeamName": playerRows[i].TeamName,
+                                    "OwnerID": playerRows[i].OwnerID,
+                                    "OwnerName": playerRows[i].OwnerName,
+                                    "TeamID": playerRows[i].TeamID,
                                     "Points": 0,
                                     "Players": []
                                 });
-                                hashed[result[i].TeamID] = true;
+                                hashed[playerRows[i].TeamID] = true;
                             }
-                            for (var x = 0; x < compiled.length; x++)
-                            {
-                                if (compiled[x].TeamID === result[i].TeamID)
-                                {
-                                    compiled[x].Players.push(result[i]);
-                                    compiled[x].Points += result[i].Points;
+                            for (var x = 0; x < compiled.length; x++) {
+                                if (compiled[x].TeamID === playerRows[i].TeamID) {
+                                    compiled[x].Players.push(playerRows[i]);
+                                    compiled[x].Points += playerRows[i].Points;
                                 }
                             }
 
                         }
-                        setMatchups(compiled);
+                        setPlayerRows(compiled);
+                        var matchupList = result.Matchups.Matchups;
+                        if (IsAuth()) {
+                            var id = JSON.parse(localStorage.getItem('discord-user')!).id;
+                            matchupList.sort((a: any, b: any) => {
+                                if (a.TeamA_OwnerName === id) {
+                                    return -1;
+                                }
+                                else if (a.TeamB_OwnerName === id) {
+                                    return -1;
+                                }
+
+                                return 0;
+                            });
+                            //console.log(matchupList[0].MatchupID);
+                        }
+                        setMatchups(matchupList);
+                        setIsLoaded(true);
+
                     }
 
                 },
@@ -76,37 +166,12 @@ function MatchupPage() {
             <PageWrapper>
                 <div className="col-start-3 col-end-7 flex flex-col">
                     <ContentPanel>
-                        <div className="py-6">Current Matchups</div>
-                        {matchups.map((matchup: any) => (
-                            <div className='pb-6'>
-                            <div className="">{matchup.TeamName} - Total: {matchup.Points.toFixed(2)}</div>
-                            <table className="flex-auto table-auto w-full border-collapse border border-stone-500">
-                            <thead>
-                                <tr>
-                                    <th className=" border border-stone-600 bg-stone-600">Player</th>
-                                    <th className=" border border-stone-600 bg-stone-600">Team</th>
-                                    <th className=" border border-stone-600 bg-stone-600">Role</th>
-                                    <th className=" border border-stone-600 bg-stone-600">Points</th>
-                                    <th className=" border border-stone-600 bg-stone-600">Result Rows</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {matchup.Players.map((item: any) => (
-                                        <tr key={"row" + item.PlayerID}>
-                                            <td key={"name" + item.PlayerID} className="border px-4 py-2 border-stone-600 font-normal"><Link to={"/player/" + item.PlayerID}><Chip>{item.PlayerName}</Chip></Link></td>
-                                            <td key={"team" + item.PlayerID} className="border px-4 py-2 border-stone-600 font-normal">{item.ProTeamTag}</td>
-                                            <td key={"role" + item.PlayerID} className="border px-4 py-2 border-stone-600 font-normal">{Role(item.FantasyRole)}</td>
-                                            <td key={"role" + item.PlayerID} className="border px-4 py-2 border-stone-600 font-normal">{item.Points}</td>
-                                            <td key={"role" + item.PlayerID} className="border px-4 py-2 border-stone-600 font-normal">{item["Result Rows"]}</td>
-                                        </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                        </div>
-
-                        ))}     
-                                 
-    
+                        <div className="pt-6">Current Matchups - Starting {new Date(matchups[0].Date).toDateString()}</div>
+                        {matchups.map((item: any, i: any) => (
+                            <div className='py-6'>
+                                {UserMatchup(i)}
+                            </div>
+                        ))}
                     </ContentPanel>
                 </div>
             </PageWrapper>
@@ -114,7 +179,7 @@ function MatchupPage() {
     }
 
 
-    
+
 }
 
 export default MatchupPage;
